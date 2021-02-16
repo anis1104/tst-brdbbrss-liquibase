6f9d5ae8a4286135b158a29de789b212d29a9c4a
Mohammed Anis Rahman <71458724+anis1104@users.noreply.github.com>
2021-02-16 17:12:38 +0000

    Create create.sql

---
+++ flyway/sql/create.sql
@@ -0,0 +1,9 @@
+
+create role aws_ext_role;
+create role agentlvrole;
+create role app_monitor;
+create role auditor;
+create role ballvrole;
+create role batchlvrole;
+create role brdb_exceptionslv;
+create role deltrrole;
